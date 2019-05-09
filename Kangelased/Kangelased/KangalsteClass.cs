972bb331b3a8d932f3c9f76f2fcebec6baf61f8b
Keiti <opilane@HP-CZC6297B8B>
2019-05-09 11:41:28 +0300

    Lahenda esimene ülesanne - Loo kangelase klass

---
+++ Kangelased/Kangelased/KangalsteClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kangelased
+{
+    class Kangelane
+    {
+        // privaatsed isendiväljad
+        private string nimi;
+        private string asukoht;
+        //konstruktor
+        public Kangelane(string nimi, string asukoht)
+        {
+            Nimi = nimi;
+            Asukoht = asukoht;
+        }
+        //•	Kõigi isendiväljade jaoks on vastavad get-meetodid ja set-meetodid.
+        public string Nimi { get => nimi; set => nimi = value; }
+        public string Asukoht { get => asukoht; set => asukoht = value; }
+
+        public int Päästa(int ohus)
+        {
+            return (int)Math.Round(ohus * .95);
+        }
+        public override string ToString()
+        {
+            return Nimi + "hoiab" +Asukoht+ "turvalisena.";
+        }
+    }
+}
